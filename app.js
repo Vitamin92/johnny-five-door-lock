920c95a0c9837c96255001f84c8a2fd012306d1e
Vitamin92 <elrimbor@gmail.com>
2018-04-25 23:26:53 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.listen(3000);
+
+//setup the ardunio for connection:
+var five = require( 'johnny-five' ),board,led,hall;
+board = new five.Board();
+   board.on("ready", function() {
+
+       led = new five.Led(10);
+       hall = new five.Sensor.Digital(2);
+       console.log("board is ready");
+//     var relay = new five.Relay(10);
+
+       });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+//app.get request on /on triggers turning on the LED
+app.get('/on',function(req,res){
+    console.log("led on");
+    if(board.isReady && !hall.value){
+        led.on();
+        setTimeout(() => {
+            led.toggle();
+    }, 200);}
+    console.log("Lock is open");
+    res.redirect('/');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
